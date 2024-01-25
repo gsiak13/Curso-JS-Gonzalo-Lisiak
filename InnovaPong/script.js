b83a4dbbb74de938c3c2652962aabe548cbfc0cc
Gonzalo Lisiak <gonzalolisiak@BA-MAC-GonzaL.local>
2024-01-25 16:02:04 -0300

    primer commit

---
+++ InnovaPong/script.js
@@ -0,0 +1,77 @@
+const participantes = []
+
+
+
+const crearJugador = () => {
+    const nombre = prompt("ingrese el nombre del participante");
+    const apellido = prompt("ingrese el apellido del participante");
+    const jugador = {nombre, apellido}
+    return jugador
+}
+
+
+
+const sumarParticipante = () => {
+    const jugador = crearJugador();
+    if(!existeParticipante(jugador)){
+        participantes.push(jugador)
+        alert("participante agregado.")
+        }else{
+            alert("el participante ya se encuentra participando del torneo.")
+        }
+}
+
+//agregar la cantidad de participantes que se desee
+function agregarParticipante(){
+    //let cantidadParticipantes = parseInt(prompt("cuantos participantes quiere agregar?"));
+    //for(let i = 0 ; i<cantidadParticipantes; i++){
+        sumarParticipante();
+    //}
+}
+
+const mostrarParticipantes = () => {
+    for (const participante of participantes) {
+        console.table(participante)
+    }
+}
+   
+//verificar si un participante ya es parte del torneo
+function existeParticipante(jugador){
+    let existe = false
+    for(let participante of participantes){
+        if(participante.nombre == jugador.nombre){
+            if(participante.apellido == jugador.apellido){
+                existe = true;
+            }
+        }
+    }
+    return existe;
+}
+
+//no funciona bien. pedir ayuda
+function jugarPartido(){
+    let jugador1 
+    let jugador2 
+    do{
+    jugador1 = crearJugador();
+    }while(!existeParticipante(jugador1))
+    const resultadoJugador1 = parseInt(prompt("ingrese los puntos del primer jugador: "))
+
+    do{
+    jugador2 = crearJugador();
+        }while(!existeParticipante(jugador2))
+        const resultadoJugador2 = parseInt(prompt("ingrese los puntos del segundo jugador: "))
+
+    if(resultadoJugador1>resultadoJugador2){
+        console.log("gano " + jugador1.nombre + " " +jugador1.apellido)
+    }else if(resultadoJugador1<resultadoJugador2){
+        console.log("gano "+ jugador2.nombre + " " +jugador2.apellido)
+    }else{
+        console.log("es empate")
+    }
+}
+
+// agregarParticipante()
+// mostrarParticipantes()
+// jugarPartido()
+
